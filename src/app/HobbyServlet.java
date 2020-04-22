f6ac15c86a9895900bea69bb42181bcdb7a1588c
ohira_kaito <ka-ohira@virtualex.co.jp>
2020-04-22 17:44:18 +0900

    17:45

--- src/app/HobbyServlet.java
+++ src/app/HobbyServlet.java
@@ -1,10 +1,18 @@
 package app;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebServlet("/api/hobby")
 public class HobbyServlet extends HttpServlet {
@@ -17,7 +25,75 @@ public class HobbyServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		// TODO 必須機能「趣味参照機能」
-		
+		String syainId=request.getParameter("syainId");
+		try {
+			// JDBCドライバのロード
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			} catch (ClassNotFoundException e) {
+			// ドライバが設定されていない場合はエラーになります
+			throw new RuntimeException(String.format("JDBCドライバのロードに失敗しました。詳細:[%s]", e.getMessage()), e);
+			}
+
+		String url = "jdbc:log4jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "wt2";
+		String pass = "wt2";
+
+		String sql="select \n" +
+				"ROWNUM NO, \n" +
+				"MC.CATEGORY_NAME, \n" +
+				"MH.HOBBY_NAME \n" +
+				"from \n" +
+				"MS_SYAIN MS, \n" +
+				"MS_SYAIN_HOBBY MSH, \n" +
+				"MS_HOBBY MH, \n" +
+				"MS_CATEGORY MC \n" +
+				"where 1=1 \n" +
+				"and MS.SYAINID='0001' \n" +
+				"and MS.SYAINID=MSH.SYAINID \n" +
+				"and MSH.HOBBY_ID=MH.HOBBY_ID \n" +
+				"and MH.CATEGORY_ID=MC.CATEGORY_ID \n" ;
+		try (
+				// データベースへ接続します
+				Connection con = DriverManager.getConnection(url, user, pass);
+
+				// SQLの命令文を実行するための準備をおこないます
+				Statement stmt = con.createStatement();
+
+				// SQLの命令文を実行し、その結果をResultSet型のrsに代入します
+				ResultSet rs1 = stmt.executeQuery(sql);) {
+
+			// 社員情報を保持するため、Employee型の変数empを宣言
+			// 変数empはJSPに渡すための社員情報を保持させます
+			Employee emp = new Employee();
+
+			// SQL実行結果を保持している変数rsから社員情報を取得
+			// rs.nextは取得した社員情報表に次の行があるとき、trueになります
+			// 次の行がないときはfalseになります
+			if (rs1.next()) {
+				emp.setId(id); // 社員IDを変数empに代入
+				emp.setName(rs1.getString("syainname"));// SQL実行結果のsyainname列の値を取得し変数empに代入します
+				emp.setAddress(rs1.getString("syusinttdfkn")); // SQL実行結果の「syusinttdfkn」列の値を取得し変数empに代入します
+				emp.setImage(rs1.getString("imagefilename")); // SQL実行結果の「imagefilename」列の値を取得し変数empに代入します
+				emp.setBirthYmd(rs1.getString("tanjyoymd")); // 以下、同様なので以下省略します
+				emp.setCollege(rs1.getString("daigakuname"));
+				emp.setMajor(rs1.getString("senkokamoku"));
+				emp.setLicense(rs1.getString("syutokusikaku"));
+				emp.setEnterYmd(rs1.getString("nyusyaymd"));
+				emp.setComment(rs1.getString("freecomment"));
+			}
+
+			// アクセスした人に応答するためのJSONを用意する
+			PrintWriter pw = response.getWriter();
+
+			// JSONで出力する
+			pw.append(new ObjectMapper().writeValueAsString(emp));
+
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細：[%s]", e.getMessage()), e);
+		}
+
+
+
 		// -- ここまで --
 	}
 
